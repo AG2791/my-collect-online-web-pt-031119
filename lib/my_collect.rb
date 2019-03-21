b3bf3dc595ad4570c7b6bb27f376ba1f22602dfa
AJ Guiles <ashiguiles@gmail.com>
2019-03-21 01:11:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,17 @@
 def my_collect(collection)
-  
+  i=
  while i < collection.length 
    yied 
    
    
-   
-   
-   
-  
-  
-end 
+   end 
+
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
